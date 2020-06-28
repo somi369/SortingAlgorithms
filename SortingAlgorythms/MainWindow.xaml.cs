0c052be56a78af99e7556ef8d0c81d466114868e
somi369 <64969317+somi369@users.noreply.github.com>
2020-06-28 15:50:38 +0200

    Generation function changed
    Values in the array are UNIQUE from now on.

--- SortingAlgorythms/MainWindow.xaml.cs
+++ SortingAlgorythms/MainWindow.xaml.cs
@@ -23,21 +23,23 @@ namespace SortingAlgorythms
     public partial class MainWindow : Window
     {
         private int[] randomNumbers = new int[20];
+
+        //----- LVC START -----
+        public SeriesCollection LiveChartValueSeries { get; set; }
+        public string[] Labels { get; set; }
+        public Func<int, string> Formatter { get; set; }
+        //*----- LVC END -----
         public MainWindow()
         {
             InitializeComponent();
             //----- LVC START-----
-            series = new SeriesCollection
+            LiveChartValueSeries = new SeriesCollection
             {
                 new ColumnSeries
                 {
-                    Values = new ChartValues<ObservableValue>
+                    Values = new ChartValues<int>
                     {
-                        new ObservableValue(3),
-                        new ObservableValue(6),
-                        new ObservableValue(7),
-                        new ObservableValue(4),
-                        new ObservableValue(2)
+                        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
                     },
                     Fill = Brushes.Black
                 }
@@ -49,142 +51,37 @@ namespace SortingAlgorythms
             DataContext = this;
             //----- LVC END -----
         }
-        //----- LVC START -----
-        public SeriesCollection series { get; set; }
-        public string[] Labels { get; set; }
-        public Func<int, string> Formatter { get; set; }
-        //*----- LVC END -----
 
-        private void MergeSortMain()
-        {
-            //MergeSort(randomNumbers);
-        }
-        /*
-        private int[] MergeSort(int[] array)
-        {
-            if (array.Length == 1)
-            {
-                return array;
-            }
-            //Declare and initialise the new half arrays
-            int[] arrayOne;
-            int[] arrayTwo;
-            if (array.Length % 2 == 0)
-            {
-                arrayOne = new int[array.Length / 2];
-                arrayTwo = new int[array.Length / 2];
-            }
-            else
-            {
-                arrayOne = new int[(array.Length / 2) + 1];
-                arrayTwo = new int[array.Length / 2];
-            }
-            for (int i = 0; i < arrayOne.Length; i++)
-            {
-                arrayOne[i] = array[i];
-            }
-            for(int i = 0; i < arrayTwo.Length ;i++)
-            {
-                arrayTwo[i]= array[arrayOne.Length+i];
-            }
-
-            arrayOne = MergeSort(arrayOne);
-            arrayTwo = MergeSort(arrayTwo);
-
-            arrayOne MergeSort()
-            return Merge(arrayOne, arrayTwo);
-        }
-        */
-
-        private int[] Merge(int[] arrayOne, int[] arrayTwo)
-        {
-            return arrayOne;
-        }
-        private void QuickSort()
-        {
-
-        }
-
-        private void BubbleSort()
-        {
-
-        }
-
-        private void HeapSort()
-        {
-
-        }
-
-        private void SwapValues(int a, int b)
-        {
-            int c = a;
-            a = b;
-            b = c;
-        }
-
+        //Generates random array with unique values
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
-            series[0].Values.Clear();
-            Random rand = new Random();
-            for (int i = 0; i < 20; i++)
+            LiveChartValueSeries[0].Values.Clear();
+
+            var Numbers = Enumerable.Range(1, 20).ToArray();
+            var Rnd = new Random();
+
+            // Shuffle the array
+            for (int i = 0; i < Numbers.Length; ++i)
             {
-                randomNumbers[i] = rand.Next(1, 20);
-                series[0].Values.Add(new ObservableValue(randomNumbers[i]));
+                int RandomIndex = Rnd.Next(Numbers.Length);
+                int Temp = Numbers[RandomIndex];
+                Numbers[RandomIndex] = Numbers[i];
+                Numbers[i] = Temp;
             }
-            //Array.Sort(randomNumbers);
 
-
-
+            // Now your array is randomized and you can simply print them in order
+            for (int i = 0;i < Numbers.Length;++i)
+            {
+                LiveChartValueSeries[0].Values.Add(Numbers[i]);
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            //Random rand = new Random();
-            //int a = rand.Next(0, 20);
-            //int b = rand.Next(0, 20);
-            //int c = Int16.Parse(series[0].Values[a].ToString());
-            //series[0].Values[a] = series[0].Values[b];
-            //series[0].Values[b] = c;
-
-            for (int i = 0; i < 20; i++)
-            {
-                for (int j = i + 1; j < 20; j++)
-                {
-                    int a = getSeriesElement(i);
-                    int b = getSeriesElement(j);
-                    if (b < a)
-                    {
-                        Dispatcher.Invoke(() =>
-                        {
-                            int c = a;
-                            setSeriesElement(i, b);
-                            setSeriesElement(j, c);
-                            System.Threading.Thread.Sleep(1000);
-                        });
-
-                    }
-                }
-
-            }
+            
 
         }
-        public int getSeriesElement(int idx)
-        {
-            int[] seriesNumbers = new int[20];
-            series[0].Values.GetEnumerator
-            return seriesNumbers[idx];
-        }
-        public void setSeriesElement(int idx, int value)
-        {
-            int[] seriesNumbers = new int[20];
-            series[0].Values.CopyTo(seriesNumbers, 0);
-            seriesNumbers[idx] = value;
-            series[0].Values.Clear();
-            for (int i = 0; i < 20; i++)
-            {
-                series[0].Values.Add(new ObservableValue(seriesNumbers[i]));
-            }
-        }
+
     }
 }
