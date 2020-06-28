cbe9554093aa9223296c2ca88592bfcf364548ff
somi369 <64969317+somi369@users.noreply.github.com>
2020-06-28 20:41:09 +0200

    BackgroundWorker update
    Added BackgroundWorker, separated initialisations

--- SortingAlgorythms/MainWindow.xaml.cs
+++ SortingAlgorythms/MainWindow.xaml.cs
@@ -1,20 +1,12 @@
 ﻿using LiveCharts;
 using LiveCharts.Wpf;
-using LiveCharts.Defaults;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
+using System.ComponentModel;
 
 namespace SortingAlgorythms
 {
@@ -22,17 +14,30 @@ namespace SortingAlgorythms
     /// 
     public partial class MainWindow : Window
     {
-        private int[] randomNumbers = new int[20];
 
-        //----- LVC START -----
+        //-----------LiveChart START-----------
         public SeriesCollection LiveChartValueSeries { get; set; }
         public string[] Labels { get; set; }
         public Func<int, string> Formatter { get; set; }
-        //*----- LVC END -----
+        //-----------LiveChart END-----------
+
+        //-----------BackgroundWorker START-----------
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+        //-----------BackgroundWorker END-----------
+
+        private int[] randomNumbers = new int[20];
+
         public MainWindow()
         {
             InitializeComponent();
-            //----- LVC START-----
+            //Livechart
+            InitializeLiveChart();
+            //BackgroundWorker
+            InitializeBackgroundWorker();
+        }
+        
+        private void InitializeLiveChart()
+        {
             LiveChartValueSeries = new SeriesCollection
             {
                 new ColumnSeries
@@ -44,20 +49,24 @@ namespace SortingAlgorythms
                     Fill = Brushes.Black
                 }
             };
-
-
             Labels = new[] { "Label1", "Label2" };
             Formatter = value => value.ToString("N");
             DataContext = this;
-            //----- LVC END -----
         }
+        private void InitializeBackgroundWorker()
+        {
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+        }
+
 
         //Generates random array with unique values
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            // Clear Chart values
             LiveChartValueSeries[0].Values.Clear();
 
+            //Create an array
             var Numbers = Enumerable.Range(1, 20).ToArray();
             var Rnd = new Random();
 
@@ -70,8 +79,8 @@ namespace SortingAlgorythms
                 Numbers[i] = Temp;
             }
 
-            // Now your array is randomized and you can simply print them in order
-            for (int i = 0;i < Numbers.Length;++i)
+            // Change Chart values
+            for (int i = 0; i < Numbers.Length; ++i)
             {
                 LiveChartValueSeries[0].Values.Add(Numbers[i]);
             }
@@ -79,8 +88,35 @@ namespace SortingAlgorythms
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            
+        
+            MessageBox.Show("burh");
+        }
 
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            worker.RunWorkerAsync();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int j = 0;
+            while (j < 5)
+            {
+                Thread.Sleep(1000);
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    //BTN.Content = "HAHA"+j;
+                    LiveChartValueSeries[0].Values[0] = j;
+                }));
+
+                j++;
+            }
+        }
+
+        private void worker_RunWorkerCompleted(object sender,
+                                                   RunWorkerCompletedEventArgs e)
+        {
+            //update ui once worker complete his work
         }
 
     }
