aaca5ad8d3d491750d7a90516e96bed685d61203
somi369 <64969317+somi369@users.noreply.github.com>
2020-06-29 19:23:51 +0200

    Background worker removed
    Found another solution.

--- SortingAlgorythms/MainWindow.xaml.cs
+++ SortingAlgorythms/MainWindow.xaml.cs
@@ -14,28 +14,17 @@ namespace SortingAlgorythms
     /// 
     public partial class MainWindow : Window
     {
-
-        //-----------LiveChart START-----------
-        public SeriesCollection LiveChartValueSeries { get; set; }
-        public string[] Labels { get; set; }
-        public Func<int, string> Formatter { get; set; }
-        //-----------LiveChart END-----------
-
-        //-----------BackgroundWorker START-----------
-        private readonly BackgroundWorker worker = new BackgroundWorker();
-        //-----------BackgroundWorker END-----------
-
-        private int[] randomNumbers = new int[20];
-
         public MainWindow()
         {
             InitializeComponent();
             //Livechart
             InitializeLiveChart();
-            //BackgroundWorker
-            InitializeBackgroundWorker();
         }
-        
+
+        //-----------LiveChart START-----------
+        public SeriesCollection LiveChartValueSeries { get; set; }
+        public string[] Labels { get; set; }
+        public Func<int, string> Formatter { get; set; }
         private void InitializeLiveChart()
         {
             LiveChartValueSeries = new SeriesCollection
@@ -53,71 +42,65 @@ namespace SortingAlgorythms
             Formatter = value => value.ToString("N");
             DataContext = this;
         }
-        private void InitializeBackgroundWorker()
-        {
-            worker.DoWork += worker_DoWork;
-            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
-        }
+        //-----------LiveChart END-----------
 
+
+        //-----------ETC START-----------
+        private int[] randomNumbers = new int[20];
+        //-----------ETC END-----------
 
         //Generates random array with unique values
+        private void UniqueValuesBTN_Click(object sender, RoutedEventArgs e)
+        {
+            //Thread UniqueValuesThread = new Thread(AddUniqueValesToChart);
+            //UniqueValuesThread.Start();
+            new Thread(() =>
+            {
+                //Button disable
+                Dispatcher.BeginInvoke(new Action(() => { UniqueValuesBTN.IsEnabled = false; }));
+                
+                // Clear Chart values
+                LiveChartValueSeries[0].Values.Clear();
+
+                //Create an array
+                var Numbers = Enumerable.Range(1, 20).ToArray();
+                var Rnd = new Random();
+
+                // Shuffle the array
+                for (int i = 0; i < Numbers.Length; ++i)
+                {
+                    int RandomIndex = Rnd.Next(Numbers.Length);
+                    int Temp = Numbers[RandomIndex];
+                    Numbers[RandomIndex] = Numbers[i];
+                    Numbers[i] = Temp;
+                }
+
+                // Change Chart values
+                for (int i = 0; i < Numbers.Length; ++i)
+                {
+                    LiveChartValueSeries[0].Values.Add(Numbers[i]);
+                }
+                //Button enable
+                Dispatcher.BeginInvoke(new Action(() => { UniqueValuesBTN.IsEnabled = true; }));
+            }).Start();
+
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // Clear Chart values
-            LiveChartValueSeries[0].Values.Clear();
 
-            //Create an array
-            var Numbers = Enumerable.Range(1, 20).ToArray();
-            var Rnd = new Random();
-
-            // Shuffle the array
-            for (int i = 0; i < Numbers.Length; ++i)
-            {
-                int RandomIndex = Rnd.Next(Numbers.Length);
-                int Temp = Numbers[RandomIndex];
-                Numbers[RandomIndex] = Numbers[i];
-                Numbers[i] = Temp;
-            }
-
-            // Change Chart values
-            for (int i = 0; i < Numbers.Length; ++i)
-            {
-                LiveChartValueSeries[0].Values.Add(Numbers[i]);
-            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-        
-            MessageBox.Show("burh");
+
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            worker.RunWorkerAsync();
+            
         }
 
-        private void worker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            int j = 0;
-            while (j < 5)
-            {
-                Thread.Sleep(1000);
-                Dispatcher.BeginInvoke(new Action(() =>
-                {
-                    //BTN.Content = "HAHA"+j;
-                    LiveChartValueSeries[0].Values[0] = j;
-                }));
-
-                j++;
-            }
-        }
-
-        private void worker_RunWorkerCompleted(object sender,
-                                                   RunWorkerCompletedEventArgs e)
-        {
-            //update ui once worker complete his work
-        }
 
     }
 }
