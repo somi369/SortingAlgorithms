1d8250682813bf1894c62b6b69975ad788ca36d9
somi369 <64969317+somi369@users.noreply.github.com>
2020-06-30 22:16:36 +0200

    Sorting function update
    Added sorting function which changes the color of the compared elements why sorting.

--- SortingAlgorythms/MainWindow.xaml.cs
+++ SortingAlgorythms/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Media;
 using System.Threading;
 using System.Windows.Threading;
 using System.ComponentModel;
+using LiveCharts.Definitions.Series;
 
 namespace SortingAlgorythms
 {
@@ -20,6 +21,7 @@ namespace SortingAlgorythms
             //Livechart
             InitializeLiveChart();
             LiveChartArray = new int[20];
+
         }
 
         //-----------LiveChart START-----------
@@ -28,17 +30,11 @@ namespace SortingAlgorythms
         public Func<int, string> Formatter { get; set; }
         private void InitializeLiveChart()
         {
-            LiveChartValueSeries = new SeriesCollection
+            LiveChartValueSeries = new SeriesCollection();
+            for (int i = 0; i < 20; i++)
             {
-                new ColumnSeries
-                {
-                    Values = new ChartValues<int>
-                    {
-                        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-                    },
-                    Fill = Brushes.Black
-                }
-            };
+                LiveChartValueSeries.Add(new ColumnSeries { Values = new ChartValues<int> { i + 1 }, Fill = Brushes.Black });
+            }
             Labels = new[] { "Label1", "Label2" };
             Formatter = value => value.ToString("N");
             DataContext = this;
@@ -59,9 +55,13 @@ namespace SortingAlgorythms
                 Dispatcher.BeginInvoke(new Action(() => { UniqueValuesBTN.IsEnabled = false; }));
 
                 // Clear Chart values
-                LiveChartValueSeries[0].Values.Clear();
+                for (int i = 0; i < 20; i++)
+                {
+                    LiveChartValueSeries[i].Values.Clear();
+                }
 
                 //Create an array
+
                 var Numbers = Enumerable.Range(1, 20).ToArray();
                 var Rnd = new Random();
 
@@ -77,7 +77,7 @@ namespace SortingAlgorythms
                 // Change Chart values
                 for (int i = 0; i < Numbers.Length; ++i)
                 {
-                    LiveChartValueSeries[0].Values.Add(Numbers[i]);
+                    LiveChartValueSeries[i].Values.Add(Numbers[i]);
                 }
                 //Button enable
                 Dispatcher.BeginInvoke(new Action(() => { UniqueValuesBTN.IsEnabled = true; }));
@@ -101,7 +101,7 @@ namespace SortingAlgorythms
             new Thread(() =>
             {
 
-                
+
             }).Start();
         }
 
@@ -121,6 +121,78 @@ namespace SortingAlgorythms
 
         }
 
+        private void SortBTN_Click(object sender, RoutedEventArgs e)
+        {
 
+            new Thread(() =>
+            {
+                for (int i = 0; i < LiveChartValueSeries.Count - 1; i++)
+                {
+                    for (int j = i + 1; j < LiveChartValueSeries.Count; j++)
+                    {
+                        int a = Convert.ToInt32(LiveChartValueSeries[i].Values[0]);
+                        int b = Convert.ToInt32(LiveChartValueSeries[j].Values[0]);
+
+                        Dispatcher.Invoke(new Action(() =>
+                        {
+                            ChangeColorToBlue((ColumnSeries)LiveChartValueSeries[i]);
+                            ChangeColorToBlue((ColumnSeries)LiveChartValueSeries[j]);
+                        }));
+                        Thread.Sleep(200);
+
+
+                        if (a > b)
+                        {
+
+                            LiveChartValueSeries[i].Values.Clear();
+                            LiveChartValueSeries[j].Values.Clear();
+
+                            LiveChartValueSeries[i].Values.Add(b);
+                            LiveChartValueSeries[j].Values.Add(a);
+
+                            Dispatcher.Invoke(new Action(() =>
+                            {
+                                    ChangeColorToGreen((ColumnSeries)LiveChartValueSeries[i]);
+                                    ChangeColorToGreen((ColumnSeries)LiveChartValueSeries[j]);                                    
+                            }));
+                            Thread.Sleep(200);                            
+                        }
+                        else
+                        {
+                            Dispatcher.Invoke(new Action(() =>
+                            {
+                                ChangeColorToRed((ColumnSeries)LiveChartValueSeries[i]);
+                                ChangeColorToRed((ColumnSeries)LiveChartValueSeries[j]);
+                            }));
+                            Thread.Sleep(200);
+                        }
+                        Dispatcher.Invoke(new Action(() =>
+                        {
+                            ChangeColorToBlack((ColumnSeries)LiveChartValueSeries[i]);
+                            ChangeColorToBlack((ColumnSeries)LiveChartValueSeries[j]);
+                        }));
+                        Thread.Sleep(200);
+                    }
+                }
+            }).Start();
+        }
+
+        private void ChangeColorToRed(ColumnSeries x)
+        {
+
+            x.Fill = Brushes.Red;
+        }
+        private void ChangeColorToGreen(ColumnSeries x)
+        {
+            x.Fill = Brushes.LightGreen;
+        }
+        private void ChangeColorToBlack(ColumnSeries x)
+        {
+            x.Fill = Brushes.Black;
+        }
+        private void ChangeColorToBlue(ColumnSeries x)
+        {
+            x.Fill = Brushes.Blue;
+        }
     }
 }
